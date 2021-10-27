2c9b2768043bd74498a955a5580a7fbe97e14d61
Koby Lewis <kobyjlewis@gmail.com>
2021-10-26 20:24:20 -0600

    add start of AddEventScreen.js

---
+++ AddEventScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export function AddEventScreen() {
+    return (
+        <Text>This is part of the AddEventScreen</Text>
+    )
+}
--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AddEventScreen } from './AddEventScreen';
 
 import { styles } from './Styles';
 import { AddEventView } from './screens/AddEvent';

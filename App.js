9e9460580a0380dbfee3b17b401d261786221b71
incendiariopelado <43940026+incendiariopelado@users.noreply.github.com>
2021-11-09 21:03:35 -0700

    Class co code

--- App.js
+++ App.js
@@ -2,20 +2,74 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { styles } from './Styles';
+import { AddEventView } from './screens/AddEvent';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const AppStack = createStackNavigator();
+const HomeTabs = createBottomTabNavigator();
+
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>This will be our Dev Club Free Food Application!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <HomeTabs.Navigator>
+      <HomeTabs.Screen
+        name="List"
+        component={ListView}
+        options={{ headerShown: false }} />
+      <HomeTabs.Screen
+        name="Map"
+        component={MapView}
+        options={{ headerShown: false }} />
+    </HomeTabs.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ListView() {
+  return (
+    <Text>This is the list view</Text>
+  )
+}
+
+function MapView() {
+  return (
+    <Text>This is the map view</Text>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator>
+        <AppStack.Screen
+          name="Home"
+          component={Home}
+          options={({ navigation }) => ({
+            headerTitle: "BYUFreeFood",
+            headerRight: () => (
+              <Button
+                style={styles.headerButton}
+                icon={
+                  <Icon
+                    name='plus'
+                    size={24}
+                    color='blue'
+                  />
+                }
+                type='clear'
+                onPress={() => navigation.navigate("AddEvent")}
+              />
+            )
+          })}
+        />
+        <AppStack.Screen
+          name="AddEvent"
+          component={AddEventView}
+        />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}

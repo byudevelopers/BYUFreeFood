c5b5a3894f98e10be90210e8456b4ceb31ecb304
Koby Lewis <kobyjlewis@gmail.com>
2021-11-30 20:48:02 -0700

    add location screen basics

--- screens/LocationSelection.js
+++ screens/LocationSelection.js
@@ -1,8 +1,55 @@
-import React from "react"
-import { Text } from "react-native-elements"
+import React, {useState} from "react"
+import { Text, View, Pressable, StyleSheet, TextInput } from "react-native"
+
+const buildings = ["TNRB", "WILK"];
 
 export default function LocationSelection() {
-    return (
-        <Text>This is the location selection</Text>
-    )
+  const [selected, setSelected] = useState("TNRB");
+  return (
+    <View>
+      <View>{
+        buildings.map(building => {
+          return (
+            <Pressable 
+              style={[
+                locationSelectionStyles.buildingButton,
+                selected == building ? locationSelectionStyles.selected : ''
+              ]}
+              onPress={() => setSelected(building)}
+            >
+              <Text style={locationSelectionStyles.buildingButtonText}>{building}</Text>
+            </Pressable>
+          )
+        })
+      }</View>
+      <TextInput
+        multiline={true}
+        placeholder="Room Number/Description of Location"
+        style={locationSelectionStyles.description}/>
+    </View>
+  )
 }
+
+const locationSelectionStyles = StyleSheet.create({
+  buildingButton: {
+    backgroundColor: "#FFF",
+    borderColor: "#777",
+    borderWidth: 1,
+    padding: 8
+  },
+  buildingButtonText: {
+    fontSize: 18
+  },
+  selected: {
+    backgroundColor: "#DDF"
+  },
+  description: {
+    marginTop: 8,
+    fontSize: 18,
+    height: 64,
+    backgroundColor: "#FFF",
+    borderColor: "#777",
+    borderWidth: 1,
+    padding: 8
+  }
+})

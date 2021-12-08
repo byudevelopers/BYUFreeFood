e1bdb361ed87d6336d2ef44f0d0437b9edc5d44e
MrCheif <63733047+MrCheif@users.noreply.github.com>
2021-12-07 21:08:47 -0700

    Trying to navigate

--- screens/ListView.js
+++ screens/ListView.js
@@ -3,17 +3,20 @@ import { FlatList, Text, View, TouchableWithoutFeedback } from "react-native";
 import { Button } from "react-native-elements/dist/buttons/Button";
 import { styles } from "../Styles";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/core";
 
 const customData = require("../fake_data.json");
 const ListStack = createStackNavigator();
 
-export function ListView({ navigation }) {
+export function ListView() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const buildings = [
     { label: "TNRB", value: "TNRB" },
     { label: "JFSB", value: "JFSB" },
   ];
+  const navigation = useNavigation();
+
   return (
     <View style={styles.listContainer}>
         <FlatList

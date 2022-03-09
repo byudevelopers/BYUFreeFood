dc0e7537e05506fff6c26499f66641afd0b9505b
JJStephens <siahstephens@gmail.com>
2022-03-08 19:37:18 -0700

    fixed it cuz it wasn't working right

--- screens/ListView.js
+++ screens/ListView.js
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 import { FlatList, Text, View } from "react-native";
 import { Button } from "react-native-elements/dist/buttons/Button";
 import { styles } from "../Styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { SectionList } from "react-native";
 const customData = require("../fake_data.json");
-var dataSortedByDate = []
+var dataSortedByDate = [];
 
 export function ListView() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
-  
+
   const buildings = [
     { label: "TNRB", value: "TNRB" },
     { label: "JFSB", value: "JFSB" },
   ];
 
   function makeListOfDays() {
-    let monday = {title:"Monday", events:[]};
-    let tuesday = {title:"Tuesday", events:[]};
-    let wednesday = {title:"Wednesday", events:[]};
-    let thursday = {title:"Thursday", events:[]};
-    let friday = {title:"Friday", events:[]};
-    let saturday = {title:"Saturday", events:[]};
-    let sunday = {title:"Sunday", events:[]};
+    let monday = { title: "Monday", data: [] };
+    let tuesday = { title: "Tuesday", data: [] };
+    let wednesday = { title: "Wednesday", data: [] };
+    let thursday = { title: "Thursday", data: [] };
+    let friday = { title: "Friday", data: [] };
+    let saturday = { title: "Saturday", data: [] };
+    let sunday = { title: "Sunday", data: [] };
     dataSortedByDate.push(sunday);
     dataSortedByDate.push(monday);
     dataSortedByDate.push(tuesday);
@@ -29,45 +31,75 @@ export function ListView() {
     dataSortedByDate.push(thursday);
     dataSortedByDate.push(friday);
     dataSortedByDate.push(saturday);
-
     for (let i = 0; i < customData.length; i++) {
-      let date = new Date(customData[i])
-      if(date.getDay == 0) {
-        dataSortedByDate[0].events.push(customData[i]);
-      }
-      else if(date.getDay == 1) {
-        dataSortedByDate[1].events.push(customData[i]);
-      }
-      else if(date.getDay == 2) {
-        dataSortedByDate[2].events.push(customData[i]);
-      }
-      else if(date.getDay == 3) {
-        dataSortedByDate[3].events.push(customData[i]);
-      }
-      else if(date.getDay == 4) {
-        dataSortedByDate[4].events.push(customData[i]);
-      }
-      else if(date.getDay == 5) {
-        dataSortedByDate[5].events.push(customData[i]);
-      }
-      else if(date.getDay == 6) {
-        dataSortedByDate[6].events.push(customData[i]);
+      console.log(i);
+      let date = new Date(customData[i]);
+      console.log(customData[i].dateTime);
+      if (date.getDay == 0) {
+        dataSortedByDate[0].data.push(customData[i]);
+      } else if (date.getDay == 1) {
+        dataSortedByDate[1].data.push(customData[i]);
+      } else if (date.getDay == 2) {
+        dataSortedByDate[2].data.push(customData[i]);
+      } else if (date.getDay == 3) {
+        dataSortedByDate[3].data.push(customData[i]);
+      } else if (date.getDay == 4) {
+        dataSortedByDate[4].data.push(customData[i]);
+      } else if (date.getDay == 5) {
+        dataSortedByDate[5].data.push(customData[i]);
+      } else if (date.getDay == 6) {
+        dataSortedByDate[6].data.push(customData[i]);
       }
     }
-
   }
 
-  
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+  makeListOfDays();
+  const DATA = [
+    {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"],
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"],
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"],
+    },
+  ];
+
+  // console.log(dataSortedByDate);
+  // console.log(DATA);
+
   return (
     <SafeAreaView style={styles.container}>
-    <SectionList
-      sections={customData}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({ item }) => <Item title={item} />}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
-    />
-  </SafeAreaView>
+      {/* <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      /> */}
+      <SectionList
+        sections={dataSortedByDate}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+    // <View></View>
   );
 }

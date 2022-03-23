39bd90d0b4b377d29d7e35a75344555765a644ed
John Ellis <jgellis63@gmail.com>
2022-03-22 23:50:11 -0600

    I FINALLY GOT IT BABY! THE ICONS CAN BE COLORIZEDgit statusgit status

--- App.js
+++ App.js
@@ -12,68 +12,73 @@ import { Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { EventDetailView } from "./screens/EventDetailView";
 import { MapViewPage } from "./screens/MapViewPage";
+// import { Text, View, Image, TouchablOpacity } from 'react-native'
 
 const AppStack = createStackNavigator();
-const HomeTabs = createBottomTabNavigator ();
-  // {
-  // List : {
-  //   screen : ListView,
-  //   navigationOptions : ()=> ({
-  //     tabBarIcon : ({tintColor}) => (
-  //       <Icon
-  //         name = "list"
-  //         color = {tintColor}
-  //         size = {24}
-  //       />
-  //     )
-  //   })
-  // },
-  // Map : {
-  //   screen : MapViewPage,
-  //   navigationOptions : ()=> ({
-  //     tabBarIcon : ({tintColor}) => (
-  //       <Icon
-  //         name = "map"
-  //         color = {tintColor}
-  //         size = {24}
-  //       />
-  //       )
-  //     })
-  //   }
-  // }, {
-  //   tabBarOptions : {
-  //     activeTintColor : "blue",
-  //     inactiveTineColor : "grey"
-  //   }
-  // });
+const HomeTabs = createBottomTabNavigator();
+// {
+// List : {
+//   screen : ListView,
+//   navigationOptions : ()=> ({
+//     tabBarIcon : ({tintColor}) => (
+//       <Icon
+//         name = "list"
+//         color = {tintColor}
+//         size = {24}
+//       />
+//     )
+//   })
+// },
+// Map : {
+//   screen : MapViewPage,
+//   navigationOptions : ()=> ({
+//     tabBarIcon : ({tintColor}) => (
+//       <Icon
+//         name = "map"
+//         color = {tintColor}
+//         size = {24}
+//       />
+//       )
+//     })
+//   }
+// }, {
+//   tabBarOptions : {
+//     activeTintColor : "blue",
+//     inactiveTineColor : "grey"
+//   }
+// });
 
 // Todo: figure out how to make the icons on the tab bar have an active tint
 
 function Home() {
-  return (
-    <HomeTabs.Navigator>
-      <HomeTabs.Screen
-        name="List"
-        component={ListView}
-        options={{
-          headerShown: false,
-          tabBarIcon : ({tintColor}) => (
-            <Icon name = "list" color = {tintColor} size = {28} />
-          ),
-        }}
-      />
-      <HomeTabs.Screen
-        name="Map"
-        component={MapViewPage}
-        options={{
-          headerShown: false,
-          tabBarIcon : ({tintColor}) => (
-            <Icon name = "map" color = {tintColor} size = {28} />
-          ),
-        }}
-      />
-    </HomeTabs.Navigator>
-  );
+    return (
+        <HomeTabs.Navigator
+            tabBarOptions={{
+                activeTintColor: 'blue',
+            }}
+        >
+            <HomeTabs.Screen
+                name="List"
+                component={ListView}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="list" color={color} size={28} />
+                    )
+                }}
+            />
+            <HomeTabs.Screen
+                name="Map"
+                component={MapViewPage}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="map" color={color} size={28} />
+                    ),
+                }}
+            />
+        </HomeTabs.Navigator>
+    );
 }
 
 // tabBarOptions: {
@@ -90,31 +95,31 @@ function Home() {
 //       },
 
 function MapView() {
-  return MapViewPage;
+    return MapViewPage;
 }
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator>
-        <AppStack.Screen
-          name="Home"
-          component={Home}
-          options={({ navigation }) => ({
-            headerTitle: "BYUFreeFood",
-            headerRight: () => (
-              <Button
-                style={styles.headerButton}
-                icon={<Icon name="plus" size={24} color="blue" />}
-                type="clear"
-                onPress={() => navigation.navigate("AddEvent")}
-              />
-            ),
-          })}
-        />
-        <AppStack.Screen name="AddEvent" component={AddEventView} />
-        <AppStack.Screen name="EventDetailView" component={EventDetailView} />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator>
+                <AppStack.Screen
+                    name="Home"
+                    component={Home}
+                    options={({ navigation }) => ({
+                        headerTitle: "BYUFreeFood",
+                        headerRight: () => (
+                            <Button
+                                style={styles.headerButton}
+                                icon={<Icon name="plus" size={24} color="blue" />}
+                                type="clear"
+                                onPress={() => navigation.navigate("AddEvent")}
+                            />
+                        ),
+                    })}
+                />
+                <AppStack.Screen name="AddEvent" component={AddEventView} />
+                <AppStack.Screen name="EventDetailView" component={EventDetailView} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
 }

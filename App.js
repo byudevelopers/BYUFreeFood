339488bef490d91d765980f673b45e7c34ebdb45
John Ellis <jgellis63@gmail.com>
2022-03-23 00:13:26 -0600

    made some minor detail changes/conflict fixes

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ const HomeTabs = createBottomTabNavigator();
 function Home() {
     return (
         <HomeTabs.Navigator
+            // tabBarOptions was all that was needed to be added to the navigator component to make the icons have active tinting
             tabBarOptions={{
                 activeTintColor: 'blue', // maybe change this color, the blue is a little annoying on the eyes
             }}
